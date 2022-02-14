4a1eedecec0d1a796072516f4f179ad27cc3726e
Marius Gžegoževskis <m.gzegozevskis@ad.viko.lt>
2022-02-14 19:28:30 +0200

    pirma paskaita

--- introduction/main.cpp
+++ introduction/main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
+using namespace std;
+
+void suma(int a, int b){
+  cout << "suma " << a + b;
+  cout << "suma " << a + b;
+  cout << "suma " << a + b;
+}
+int add(int a, int b){
+  cout << "ADD FUNKCIJA " << a + b << endl;
+  return a + b;
+}
+int add(char a, int b){
+  return 'd';
+}
+
+
+double add(double c, int a){
+  return c + a;
+}
+
+namespace policija{
+  void spausdinti(int skaicius){
+    cout <<"Policijos funkcija Mano skaicius: "<< skaicius << endl;
+  }
+}
+namespace eif{
+  void spausdinti(int skaicius){
+    cout <<"Eif Mano skaicius: "<< skaicius << endl;
+    cout << skaicius << endl;
+  }
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+
+  int a = 10;
+  int b = 20;
+  //int suma = a + b;
+
+  suma(12, 55);
+
+
+  int rezultatas = add(77, 99);
+  double kita = add(2.5, 48);
+
+  policija::spausdinti(add(3.5, 40));
+
+
+  int skaicius = 10; // sveikasis
+  char simbolis = '#'; // 35
+  float su_kableliu = 4.55f;
+  string vardas ="Marius";
+  skaicius = 22;
+  string tekstas = "hello world";
+
+   //policija::spausdinti(suma);
+   //eif::spausdinti(33);
+
+  //cout << "Hello, World!" << endl;
   return 0;
 }
